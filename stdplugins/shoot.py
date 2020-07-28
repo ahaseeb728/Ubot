0821cd6d6fb98968aa972029c4350948955e4d6c
ahaseeb728 <63292031+ahaseeb728@users.noreply.github.com>
2020-07-29 04:04:45 +0800

    Add files via upload

---
+++ stdplugins/shoot.py
@@ -0,0 +1,30 @@
+""" .shoot """
+import sys
+from telethon import events, functions, __version__
+from uniborg.util import admin_cmd
+import asyncio
+import random
+
+KILLSTR = [
+    "AWM",
+    "M24",
+    "Kar98",
+    "AKM",
+    "M416",
+    "UZI",
+    "UMP9",
+    "Knife",
+    "Diamond Sword",
+    "Pan",
+]    
+
+
+@borg.on(admin_cmd(pattern="shoot$ ?(.*)", allow_sudo=True))  # pylint:disable=E0602
+async def killing (killed):
+    """ Dont Kill Too much -_-"""
+    if not killed.text[0].isalpha() and killed.text[0] not in ("/", "#", "@", "!"):
+        if await killed.get_reply_message():
+            await killed.edit(
+                f"Targeted user killed with {random.choice(KILLSTR)} 😈......\n"
+  "#Sad_Reacts_Onli\n"
+            )
---
+++ stdplugins/spam.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.b (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+from asyncio import wait
+from uniborg.util import admin_cmd
+from telethon import events
+
+""" .spam <n> <message>
+.spamstkr <n>"""
+
+
+@borg.on(events.NewMessage(pattern=r"\.spam", outgoing=True))
+async def spammer(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        counter = int(message[6:8])
+        spam_message = str(e.text[8:])
+
+        await wait(
+            [e.respond(spam_message) for i in range(counter)]
+            )
+
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP,
+                "#SPAM \n\n"
+                "Spam was executed successfully"
+                )
+
+@borg.on(admin_cmd(pattern="spamstkr ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = int(event.pattern_match.group(1))
+    replied = event.reply_to_msg_id
+    if not replied:
+    	await even.edit("Reply to a sticker you idiot.")
+    	return
+    reply = await event.get_reply_message()
+    stickerid = reply.file.id
+    await event.delete()
+    for i in range(input_str):
+        await event.client.send_file(event.chat_id, stickerid)
